5a1ddaf9ae8c20027787583a0dd6b386d8a2a769
Luiz Serão <luiz.serao@hotmail.com>
2024-07-09 07:07:15 -0300

    página atual dinâmica

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php include('config.php'); ?>
 <?php include('header.php') ?>
 
+<?php
+  $url = isset($_GET['url']) ? $_GET['url'] : 'home';
+  if (file_exists('pages/'.$url.'.php')) {
+    include('pages/'.$url.'.php');
+  } else {
+    include('pages/home.php');
+  }
+?>
 
 <?php include('footer.php') ?>
