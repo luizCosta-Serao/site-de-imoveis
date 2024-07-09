40e9807211d5a098013b6b82fda19762b5e662c7
Luiz Serão <luiz.serao@hotmail.com>
2024-07-09 07:02:40 -0300

    header

--- header.php
+++ header.php
@@ -3,7 +3,20 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo INCLUDE_PATH ?>css/style.css">
   <title>Inovel - Imóveis</title>
 </head>
 <body>
   
+<header class="header">
+  <section class="container">
+    <div class="logo">Inovel</div>
+    <nav class="menu">
+      <ul>
+        <li><a href="">Centro Empresarial 1</a></li>
+        <li><a href="">Centro Empresarial 2</a></li>
+        <li><a href="">Centro Empresarial 3</a></li>
+      </ul>
+    </nav>
+  </section>
+</header>
