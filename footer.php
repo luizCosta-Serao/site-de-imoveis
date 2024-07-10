04982aa1f7417f979f32dafa5947ad3259bb2930
Luiz Serão <luiz.serao@hotmail.com>
2024-07-09 21:50:36 -0300

    front end  do site de imóveis

--- footer.php
+++ footer.php
@@ -1,2 +1,5 @@
+<script src="<?php echo INCLUDE_PATH_PAINEL; ?>js/jquery.js"></script>
+<script src="<?php echo INCLUDE_PATH_PAINEL; ?>js/plugins/jquery.maskMoney.js"></script>
+<script src="<?php echo INCLUDE_PATH; ?>js/index.js"></script>
 </body>
 </html>
---
+++ pages/home.php
@@ -0,0 +1,13 @@
+<div class="container">
+  <section class="lista-imoveis">
+    <h2>Listando 100 imóveis</h2>
+    <div class="single-imovel">
+        <img src="<?php echo INCLUDE_PATH_PAINEL ?>/uploads/668b028a4d311.jpeg" alt="">
+        <div>
+          <p>Nome do Imóvel: Empreendimento</p>
+          <p>Área do Imóvel: 100m2</p>
+          <p>Preço do Imóvel: R$ 4.000,00</p>
+        </div>
+    </div>
+  </section>
+</div>
