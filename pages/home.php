da9a7f83acdd424143a907fb6453c55fb570a4c6
Luiz Serão <luiz.serao@hotmail.com>
2024-07-10 07:43:05 -0300

    menu dinâmico e mostrando imóveis obtidos do banco de dados

--- pages/home.php
+++ pages/home.php
@@ -1,13 +1,48 @@
 <div class="container">
   <section class="lista-imoveis">
-    <h2>Listando 100 imóveis</h2>
+    <?php
+      if (!isset($_GET['empreendimento'])) {
+        // Se parâmetro empreendimento nao estiver setado
+        // Selecionar todos os imóveis
+        $selectImoveis = MySql::connect()->prepare("SELECT * FROM `imoveis`");
+        $selectImoveis->execute();
+        $imoveis = $selectImoveis->fetchAll();
+        echo '<h2>Listando '.count($imoveis).' imóveis</h2>';
+      } else {
+        // Se parâmetro empreendimento estiver setado
+        // Obter o valor de GET empreendimento (slug)
+        $slugEmpreendimento = $_GET['empreendimento'];
+        // Obter o empreendimento
+        $empreendimento = MySql::connect()->prepare("SELECT * FROM `empreendimentos` WHERE slug = ?");
+        $empreendimento->execute(array($slugEmpreendimento));
+        // Se existir empreendimento
+        if ($empreendimento->rowCount() === 1) {
+          // Obter os imóveis que estão no empreendimento
+          $empreendimento = $empreendimento->fetch();
+          $selectImoveis = MySql::connect()->prepare("SELECT * FROM `imoveis` WHERE empreendimento_id = ?");
+          $selectImoveis->execute(array($empreendimento['id']));
+          $imoveis = $selectImoveis->fetchAll();
+          echo '<h2>Listando '.count($imoveis).' imóveis</h2>';
+        }
+      }
+
+      // Iterando os imóveis
+      foreach ($imoveis as $key => $value) {
+    ?>
     <div class="single-imovel">
-        <img src="<?php echo INCLUDE_PATH_PAINEL ?>/uploads/668b028a4d311.jpeg" alt="">
+        <?php
+          // Obter as imagens do imóvel
+          $imagensImovel = MySql::connect()->prepare("SELECT * FROM `imagens_imovel` WHERE imovel_id = ? LIMIT 1");
+          $imagensImovel->execute(array($value['id']));
+          $imagensImovel = $imagensImovel->fetch();
+        ?>
+        <img src="<?php echo INCLUDE_PATH_PAINEL ?>/uploads/<?php echo $imagensImovel['imagem'] ?>" alt="">
         <div>
-          <p>Nome do Imóvel: Empreendimento</p>
-          <p>Área do Imóvel: 100m2</p>
-          <p>Preço do Imóvel: R$ 4.000,00</p>
+          <p>Nome do Imóvel: <?php echo $value['nome'] ?></p>
+          <p>Área do Imóvel: <?php echo $value['area'] ?></p>
+          <p>Preço do Imóvel: R$ <?php echo Painel::convertMoney($value['preco']); ?></p>
         </div>
     </div>
+    <?php } ?>
   </section>
 </div>
